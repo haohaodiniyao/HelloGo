b8d9ec3946ea2821b47869f0ae781bad39256c06
yaokai <yaokai@ushareit.com>
2022-04-05 19:23:54 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"HelloGo/controller"
+	"HelloGo/tool"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg, err := tool.ParseConfig("./config/app.json")
+	if err != nil{
+		panic(err.Error())
+	}
+	engine := gin.Default()
+	registerRouter(engine)
+	engine.Run(cfg.AppHost+":"+cfg.AppPort)
+}
+//路由
+func registerRouter(router *gin.Engine) {
+	new(controller.HelloController).Router(router)
+}
---
+++ tool/Config.go
@@ -0,0 +1,29 @@
+package tool
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	AppName string `json:"app_name"`
+	AppMode string `json:"app_mode"`
+	AppHost string `json:"app_host"`
+	AppPort string `json:"app_port"`
+}
+var _cfg *Config = nil
+func ParseConfig(path string) (*Config,error)  {
+	file, err := os.Open(path)
+	if err!=nil{
+		panic(err)
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	decoder := json.NewDecoder(reader)
+	err = decoder.Decode(&_cfg)
+	if err != nil{
+		return nil,err
+	}
+	return _cfg,nil
+}
